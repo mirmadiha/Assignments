f0499eefaa3e79d09b17db9d8b0891337a143ddb
Madiha Aijaz <mirmadihaaijaz@gmail.com>
2025-11-23 23:37:10 +0530

    further updates

--- A-4/routes/shop.js
+++ A-4/routes/shop.js
@@ -1,8 +1,6 @@
 const express=require('express');
 const router=express.Router();
 
-const rootDir=require('../util/path');
-
 router.get('/',(req,res,next)=>{
     res.render('shop',{pageTitle:'Shop', path:'/'});
 })
